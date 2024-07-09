42f4e6b5ff6ac5b2a8531fff3aef40effc8a83b9
John Amores <kayak.stated_0b@icloud.com>
2024-07-09 17:39:58 -0400

    Refactored code

--- components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-    return (
-      <footer className="bg-dark-400 text-white py-4 mt-auto">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2024 CarePulse. All rights reserved.</p>
-        </div>
-      </footer>
-    );
-  }
